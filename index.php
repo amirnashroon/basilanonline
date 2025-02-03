92d4b73cc5eb05def897fce5f88db6cec6fe10b8
amirnashroon <amiribno01@gmail.com>
2025-02-03 04:20:29 -0800

    hello

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basilan Online</title>
-    <link rel="stylesheet" href="style10.css">
+    <link rel="stylesheet" href="style100.css">
 </head>
 <body>
     <header>
